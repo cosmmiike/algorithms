69cdea5cce7f8de7d68d71afda1fae4241bb15a1
cosmmiike <mikhail.shaym@gmail.com>
2020-05-12 18:06:35 +0200

    Olimpiada Results Task

---
+++ 1. Сортировки. Теория/solutions/1D.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <random>
+using namespace std;
+
+
+struct participant {
+  int id;
+  int result;
+};
+
+
+void participant_sort(vector<participant> &p) {
+  for (int i = 0; i < p.size() - 1; i++)
+    for (int j = 1; j < p.size() - i; j++)
+      if (p[j - 1].result < p[j].result)
+        swap(p[j - 1], p[j]);
+      else if (p[j - 1].result == p[j].result && p[j - 1].id > p[j].id) {
+        swap(p[j - 1], p[j]);
+      }
+}
+
+
+vector<int> generate_array(int size) {
+  vector<int> v;
+
+  srand(unsigned(time(0)));
+  for (int i = 0; i < size; i++)
+    v.push_back(rand() % 101 - 50);
+
+  return v;
+}
+
+
+vector<participant> read_data() {
+  vector<participant> participants;
+  participant p;
+  int n;
+
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> p.id >> p.result;
+    participants.push_back(p);
+  }
+
+  return participants;
+}
+
+
+void print_data(vector<participant> &v) {
+  for (auto e: v)
+    cout << e.id << " " << e.result << endl;
+}
+
+
+int main() {
+  vector<participant> v;
+
+  v = read_data();
+  participant_sort(v);
+  print_data(v);
+
+  return 0;
+}
