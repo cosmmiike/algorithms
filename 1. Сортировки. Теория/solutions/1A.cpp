40913630aa79ddb8190da0b985e691443080ffc6
cosmmiike <mikhail.shaym@gmail.com>
2020-05-12 15:18:26 +0200

    Improvements

--- 1. Сортировки. Теория/solutions/1A.cpp
+++ 1. Сортировки. Теория/solutions/1A.cpp
@@ -5,10 +5,8 @@ using namespace std;
 
 
 void bubble_sort(vector<int> &v) {
-  int i, j, temp;
-
-  for (i = 0; i < v.size() - 1; i++)
-    for (j = 1; j < v.size() - i; j++)
+  for (int i = 0; i < v.size() - 1; i++)
+    for (int j = 1; j < v.size() - i; j++)
       if (v[j - 1] > v[j])
         swap(v[j - 1], v[j]);
 }
@@ -16,10 +14,9 @@ void bubble_sort(vector<int> &v) {
 
 vector<int> generate_array(int size) {
   vector<int> v;
-  int i, r;
-  srand(unsigned(time(0)));
 
-  for (i = 0; i < size; i++)
+  srand(unsigned(time(0)));
+  for (int i = 0; i < size; i++)
     v.push_back(rand() % 101 - 50);
 
   return v;
@@ -28,10 +25,10 @@ vector<int> generate_array(int size) {
 
 vector<int> read_array() {
   vector<int> v;
-  int i, n, e;
+  int n, e;
 
   cin >> n;
-  for (i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++) {
     cin >> e;
     v.push_back(e);
   }
@@ -41,18 +38,14 @@ vector<int> read_array() {
 
 
 void print_array(vector<int> &v) {
-  int i;
-
   for (auto e: v)
     cout << e << " ";
-
   cout << endl;
 }
 
 
 int main() {
   vector<int> v;
-  int i, n;
 
   v = read_array();
   bubble_sort(v);
--- 1. Сортировки. Теория/solutions/sort_array.cpp
+++ 1. Сортировки. Теория/solutions/sort_array.cpp
@@ -5,10 +5,8 @@ using namespace std;
 
 
 void sort_array(vector<int> &v) {
-  int i, j, temp;
-
-  for (i = 0; i < v.size() - 1; i++)
-    for (j = i + 1; j < v.size(); j++)
+  for (int i = 0; i < v.size() - 1; i++)
+    for (int j = i + 1; j < v.size(); j++)
       if (v[i] > v[j])
         swap(v[i], v[j]);
 }
@@ -16,21 +14,21 @@ void sort_array(vector<int> &v) {
 
 vector<int> generate_array(int size) {
   vector<int> v;
-  int i, r;
-  srand(unsigned(time(0)));
 
-  for (i = 0; i < size; i++)
+  srand(unsigned(time(0)));
+  for (int i = 0; i < size; i++)
     v.push_back(rand() % 101 - 50);
 
   return v;
 }
 
 
-vector<int> read_array(int size) {
+vector<int> read_array() {
   vector<int> v;
-  int i, e;
+  int n, e;
 
-  for (i = 0; i < size; i++) {
+  cin >> n;
+  for (int i = 0; i < n; i++) {
     cin >> e;
     v.push_back(e);
   }
@@ -40,8 +38,6 @@ vector<int> read_array(int size) {
 
 
 void print_array(vector<int> &v) {
-  int i;
-
   for (auto e: v)
     cout << e << " ";
 
@@ -50,11 +46,8 @@ void print_array(vector<int> &v) {
 
 
 int main() {
-  int i, n;
-  cin >> n;
-  vector<int> v = read_array(n);
+  vector<int> v = read_array();
 
-  print_array(v);
   sort_array(v);
   print_array(v);
 
