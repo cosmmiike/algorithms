584ff8addd2c1b0acfec42a894a8a69468272e95
cosmmiike <mikhail.shaym@gmail.com>
2020-05-12 20:04:23 +0200

    Removed unused libraries

--- 1. Сортировки. Теория/solutions/1D.cpp
+++ 1. Сортировки. Теория/solutions/1D.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <random>
 using namespace std;
 
 
--- 1. Сортировки. Теория/solutions/1E.cpp
+++ 1. Сортировки. Теория/solutions/1E.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <array>
-#include <random>
 using namespace std;
 
 
